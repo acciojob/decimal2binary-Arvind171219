f7f56f2d86460027e58f1459f80f2be888cea9c1
Arvind kumar <74968170+Arvind171219@users.noreply.github.com>
2023-06-08 16:47:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function threeSum(arr, target) {
+function threeSum(decimalNumber) {
 // write your code here
-  
+ let binaryNumber = "";
+
+  while (decimalNumber > 0) {
+    binaryNumber = (decimalNumber % 2) + binaryNumber;
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+  return binaryNumber;
 }
 
 module.exports = threeSum;
